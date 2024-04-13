e58319ddbef633d13870b4792f6dea8148290276
S-Farhan05 <159949925+S-Farhan05@users.noreply.github.com>
2024-04-13 21:37:55 +0500

    Created a number gussing game

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const initialNumber= Math.floor(Math.random()*6+1)
+const result = await inquirer.prompt([
+
+{
+ name:"userGuessNumber",
+ type:"number",
+ message:"Enter a number between 1 and 6 :" }
+
+])
+
+if(result.userGuessNumber>6||result.userGuessNumber<1)
+
+  {
+    console.log("Wrong input!! Pls give a number between 1 and 6");
+  }
+
+else if(result.userGuessNumber===initialNumber)
+    {
+
+         console.log("CONGRATS YOU WON THE GAME!!");
+    }
+
+    else(
+        console.log("TRY AGAIN YOU LOST")
+    )
+
+   
